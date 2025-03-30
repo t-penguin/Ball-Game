bdef1e3207c7d1ba526e9ced2dc17741cc058816
Joseph Collado <sourpina@gmail.com>
2025-03-30 00:56:03 -0400

    Implemented Timer to destroy ball
    Also added the following features:
    - Random color on spawn
    - Increasing speed while hovering with cursor

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,17 +1,24 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Ball : MonoBehaviour
 {
+    private Timer _lifeTimer;
+    [SerializeField] private float _lifetime;
     [SerializeField] private int _bounces;
-    private int _maxBounces;
+    [SerializeField] private int _maxBounces;
+
+    private Rigidbody2D _rb;
+    private bool _increaseSpeed;
 
     // Sets the ball's size and moves it in a random direction at a random speed
     void Start()
     {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
         float scalar = Random.Range(0.5f, 2f);
         transform.localScale *= scalar;
-        rb.mass *= scalar;
+        _rb.mass *= scalar;
 
         _maxBounces = Random.Range(20, 41);
 
@@ -19,7 +26,28 @@ public class Ball : MonoBehaviour
         float magnitude = Random.Range(12f, 20f);
         float x = Mathf.Cos(angle) * magnitude;
         float y = Mathf.Sin(angle) * magnitude;
-        rb.AddForce(new Vector2(x, y));
+        _rb.AddForce(new Vector2(x, y));
+
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        float r = Random.Range(0f, 1f);
+        float g = Random.Range(0f, 1f);
+        float b = Random.Range(0f, 1f);
+        sr.color = new Color(r, g, b);
+
+        _lifeTimer = gameObject.AddComponent<Timer>();
+        _lifeTimer.Duration = _lifetime;
+        _lifeTimer.Run();
+
+        _increaseSpeed = false;
+    }
+
+    private void Update()
+    {
+        if (_increaseSpeed)
+            _rb.linearVelocity *= 1.01f;
+
+        if (_lifeTimer.Finished)
+            Destroy(gameObject);
     }
 
     // Counts the number of bounces and destroys the ball after maxBounces
@@ -29,5 +57,17 @@ public class Ball : MonoBehaviour
 
         if (_bounces >= _maxBounces)
             Destroy(gameObject);
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("Hovering");
+        _increaseSpeed = true;
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("Stopped Hovering");
+        _increaseSpeed = false;
     }
 }
